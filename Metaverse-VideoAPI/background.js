6a805cf15c3fc9e89075a2597174b709d6717b4f
ogamacheDev <dev@ogamache.com>
2022-09-27 00:36:03 -0400

    Added copy to clipboard

--- Metaverse-VideoAPI/background.js
+++ Metaverse-VideoAPI/background.js
@@ -1,4 +1,4 @@
-const getVideoAndSend = () => {
+const getVideoAndSend = (destination) => {
   let videoUrl = document.querySelector("video")?.src || "";
 
   if (videoUrl.startsWith("blob:")) {
@@ -8,26 +8,35 @@ const getVideoAndSend = () => {
   }
 
   if (videoUrl) {
-    fetch("http://localhost:44913/play-video", {
-      body: videoUrl,
-      method: "POST",
-    })
-      .then((response) => {
-        console.log("Video sent to Metaverse!");
+    if (destination == "VR") {
+      fetch("http://localhost:44913/play-video", {
+        body: videoUrl,
+        method: "POST",
       })
-      .catch((error) => {
+        .then((response) => {
+          console.log("Video sent to Metaverse!");
+        })
+        .catch((error) => {
+          navigator.clipboard.writeText(videoUrl).then(() => {
+            console.error("Failed to send video to VR: ", error);
+            window.alert(
+              "Failed to send video to VR.\nCopied to clipboard instead."
+            );
+          });
+        });
+    } else if (destination == "Clipboard") {
+      setTimeout(() => {
         navigator.clipboard.writeText(videoUrl).then(() => {
-          console.error("Failed to send video to VR: ", error);
           window.alert(
-            "Failed to send video to VR.\nCopied to clipboard instead."
+            "Copied to clipboard!"
           );
         });
-      });
+      }, 500);
+    }
   }
 };
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.msg == "sendToMetaverse") {
     chrome.tabs.query(
       {
         lastFocusedWindow: true,
@@ -37,8 +46,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         chrome.scripting.executeScript({
           target: { tabId: tabs[0].id },
           function: getVideoAndSend,
+          args: [request.destination]
         });
       }
     );
-  }
 });
--- Metaverse-VideoAPI/popup.js
+++ Metaverse-VideoAPI/popup.js
@@ -2,8 +2,14 @@
 const bgPage = chrome.extension.getBackgroundPage();
 
 let metaverseButton = document.querySelector("#metaverseButton");
+let clipboardButton = document.querySelector("#clipboardButton");
 
 metaverseButton.addEventListener("click", () => {
     window.close();
-    chrome.runtime.sendMessage({ msg: "sendToMetaverse" });
+    chrome.runtime.sendMessage({ destination: "VR" });
+});
+
+clipboardButton.addEventListener("click", () => {
+    window.close();
+    chrome.runtime.sendMessage({ destination: "Clipboard" });
 });
